81ec368a5cc73bc7d84b6d3cd27f4a4ddbc1769c
Artem Yadelskyi <mymmrac@gmail.com>
2024-09-03 20:44:15 +0300

    :video_game: Removed unused tileset

--- assets/assets_release.go
+++ assets/assets_release.go
@@ -4,7 +4,10 @@ package assets
 
 import "embed"
 
-//go:embed *
+//go:embed world/*.bin
+//go:embed images/Room_Builder_32x32.png
+//go:embed images/Theme_Sorter_Black_Shadow_32x32/1_Generic_Black_Shadow_32x32.png
+//go:embed images/Premade_Character_32x32_19.png
 var embeddedFS embed.FS
 
 func init() {
